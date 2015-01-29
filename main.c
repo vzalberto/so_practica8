ea8c434f3eef0038633677013a600eec231b6f79
alumno <vzalberto@msn.com>
2015-01-28 21:34:35 -0600

    Pregunta 2

--- main.c
+++ main.c
@@ -16,7 +16,7 @@ int main(void){
 	printf("tamaño en bits de $puerto: %d\n", ((int) sizeof(puerto)) * 8);
 	printf("$puerto: %x \t(antes de htons)\n", puerto);
 
-	bzero((char*)&server_addr, sizeof(server_addr));
+	//bzero((char*)&server_addr, sizeof(server_addr));
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_addr.s_addr = INADDR_ANY;
 	server_addr.sin_port = htons(puerto);
@@ -27,6 +27,7 @@ int main(void){
 	clilen = sizeof(msg_to_client_addr);
 
 	while(1){
+		printf("Esperando cliente...\n");
 		recvfrom(s, (char *) num, 2*sizeof(int), 0, (struct sockaddr *)&msg_to_client_addr, &clilen);
 
 		res = num[0] + num[1];
